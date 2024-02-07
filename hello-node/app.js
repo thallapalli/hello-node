fb5e28acf3f1972b5109e078fcfdf679fe2c63e0
karnakar <thallapallikarnakar@gmail.com>
2024-02-06 17:54:25 -0800

    files are added

--- hello-node/app.js
+++ hello-node/app.js
@@ -1,12 +1,33 @@
 const http = require('http')
+const fs = require('fs')
 const PORT = 3000;
 const server = http.createServer((req, resp) => {
-    resp.statusCode=200;
-    resp.setHeader('Content-Type','text/plain');
-    resp.end('Hello World');
+    switch (req.url) {
+        case '/':
+            fs.readFile('./index.html', (error, data) => {
+                resp.statusCode = 200;
+                resp.setHeader('Content-Type', 'text/html');
+                resp.end(data);
+            })
+            break
+        case '/hello':
+            fs.readFile('./hello.html', (error, data) => {
+                resp.statusCode = 200;
+                resp.setHeader('Content-Type', 'text/html');
+                resp.end(data);
+            })
+            break
+        default:
 
+            resp.statusCode = 404;
+            resp.setHeader('Content-Type', 'text/html');
+            resp.end('NOT FOUND');
+
+
+
+    }
 })
 
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log('Server is running');
 })
